504629afe2d7798dc3b2a7275eddf8ae5aef00d6
Deeksha <smdeeksha2005@gmail.com>
2026-03-25 17:31:37 +0530

    [Deeksha]: UC6: Integrated Lombok for DTO

--- src/main/java/com/bridgelabz/EmployeePayroll_App/dto/EmployeeDTO.java
+++ src/main/java/com/bridgelabz/EmployeePayroll_App/dto/EmployeeDTO.java
@@ -1,22 +1,9 @@
 package com.bridgelabz.EmployeePayroll_App.dto;
 
-public class EmployeeDTO {
+import lombok.Data;
 
+@Data
+public class EmployeeDTO {
     private String name;
     private double salary;
-
-    public EmployeeDTO() {}
-
-    public EmployeeDTO(String name, double salary) {
-        this.name = name;
-        this.salary = salary;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
 }
